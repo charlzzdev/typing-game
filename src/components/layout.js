51145fab14e85ce3c007aef956422d731ca46731
Charles <ellerkaresz@gmail.com>
2019-04-10 21:27:56 +0200

    add page transition

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,13 @@
 import React from "react"
 import PropTypes from "prop-types"
+import PageTransition from 'gatsby-plugin-page-transitions'
 
 import "./layout.css"
 
 const Layout = ({ children }) => (
-      <>
+      <PageTransition>
             {children}
-      </>
+      </PageTransition>
 )
 
 Layout.propTypes = {
