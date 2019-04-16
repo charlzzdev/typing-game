cdb975931908992174a17cf67636607655a6cec8
Charles <ellerkaresz@gmail.com>
2019-04-16 15:20:04 +0200

    add Timer

---
+++ src/components/timer.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Timer({ mode, duration }) {
+      const [time, setTime] = useState({ min: 5, sec: 0 });
+
+      useEffect(() => {
+            const countdown = setInterval(() => {
+                  setTime(prevTime => {
+                        if (prevTime.sec > 0) {
+                              return {
+                                    min: prevTime.min,
+                                    sec: prevTime.sec - 1
+                              }
+                        } else if (prevTime.min !== 0) {
+                              return {
+                                    min: prevTime.min - 1,
+                                    sec: 59
+                              }
+                        } else {
+                              clearInterval(countdown);
+                              return prevTime;
+                        }
+                  });
+            }, 1000);
+
+            return () => clearInterval(countdown);
+      }, []);
+
+      return (
+            <>
+                  {
+                        mode === 'Timed' ? (
+                              <div>
+                                    {time.min < 10 ? `0${time.min}` : time.min}:{time.sec < 10 ? `0${time.sec}` : time.sec}
+                              </div>
+                        ) : <div style={{ fontSize: '40px' }}>∞</div>
+                  }
+            </>
+      )
+}
--- src/pages/game.js
+++ src/pages/game.js
@@ -3,15 +3,25 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Timer from '../components/timer'
 
 const Game = ({ location }) => {
+      if (!location.state) {
+            location.state = {
+                  gameSettings: {
+                        mode: 'Timed',
+                        type: 'Text',
+                        duration: 'Long'
+                  }
+            }
+      }
+      const gameSettings = location.state.gameSettings;
+
       return (
             <Layout>
                   <SEO title="Typing Game" />
                   <h1>Game page</h1>
-                  <p>
-                        Game started in <strong>{location.state ? location.state.gameSettings.mode : 'Timed'}</strong> mode, <strong>{location.state ? location.state.gameSettings.type : 'Text'}</strong> type and <strong>{location.state ? location.state.gameSettings.duration : 'Long'}</strong> duration.
-                  </p>
+                  <Timer mode={gameSettings.mode} duration={gameSettings.duration} />
                   <Link to="/" className="btn">Go back to the homepage</Link>
             </Layout>
       )
