39f53960b77483388d918b3082cf84d624e0431a
Charles <ellerkaresz@gmail.com>
2019-04-18 16:59:00 +0200

    style words and cursor

--- src/components/words.js
+++ src/components/words.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import wordList from '../assets/wordList';
 
@@ -6,6 +6,10 @@ export default function Words({ score, setScore }) {
       const newWord = () => wordList[Math.floor(Math.random() * 2338)];
       const [currentWord, setCurrentWord] = useState(newWord());
       const [currentChar, setCurrentChar] = useState(0);
+
+      useEffect(() => {
+            document.querySelector('.words').focus();
+      }, [])
 
       const handleKeyPress = e => {
             if (e.key === currentWord.charAt(currentChar)) {
@@ -21,10 +25,13 @@ export default function Words({ score, setScore }) {
 
       return (
             <div
+                  className="words"
                   tabIndex="0"
                   onKeyPress={(e) => handleKeyPress(e)}
             >
-                  {currentWord}
+                  {currentWord.substring(0, currentChar)}
+                  <div className="cursor"></div>
+                  <div style={{ opacity: 0.5 }}>{currentWord.substring(currentChar, currentWord.length)}</div>
             </div>
       )
 }
